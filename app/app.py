699bdd9e68872a2c64a3bb75a19294ef514ac2dd
Robin Lucbernet <robinlucbernet@gmail.com>
2017-02-12 16:55:22 +0100

    [WIP] threaded flask

--- app/app.py
+++ app/app.py
@@ -118,4 +118,4 @@ def update():
     return "OK {}".format(request.args)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
